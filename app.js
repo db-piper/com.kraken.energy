de3142a16d8177b19351f067f26be3cdaf365aa5
db-piper <202308407+db-piper@users.noreply.github.com>
2025-10-22 20:01:51 +0100

    Explicit reference to function for handlers rather than quoted strings

--- app.js
+++ app.js
@@ -25,19 +25,27 @@ module.exports = class krakenApp extends Homey.App {
    */
   async onInit() {
     this.homey.log('krakenApp.onInit: App has been initialized');
-    this.homey.log(`krakenApp.onInit: Registering run listener`);
-    this.registerConditionRunListener('slot_relative_price', productTariff.prototype.getCurrentlyCheaper.name)
+    this.registerConditionRunListener('slot_relative_price', productTariff.prototype.getCurrentlyCheaper)
 	}
 
   /**
    * Register the named function on the device class as the listener for the named condition flow card 
-   * @param {string} cardName 
-   * @param {string} functionName 
+   * @param {string}   cardName       The name of the condition card getting the listener
+   * @param {function} handlerFunction   The name of the function
    */
-  registerConditionRunListener(cardName, functionName) {
-    this.homey.flow.getConditionCard(cardName).registerRunListener(this.runListenerExecutor.bind(this, functionName));
+  registerConditionRunListener(cardName, handlerFunction) {
+    this.homey.log(`krakenApp.registerConditionRunListener: card ${cardName} function: ${handlerFunction.name}`);
+    //TODO: Research if the function reference can be passed rather than the function name
+    this.homey.flow.getConditionCard(cardName).registerRunListener(this.runListenerExecutor.bind(this, handlerFunction.name));
   }
 
+  /**
+   * Run the named function in the context of the class specific in args.device with args as parameter
+   * @param {string} functionName   The name of the handler function
+   * @param {object} args           args.device is the device instance 
+   * @param {object} state          Current homey state 
+   * @returns 
+   */
   async runListenerExecutor(functionName, args, state) {
     this.homey.log(`krakenApp.runListenerExecutor: ${functionName}`)
     const result = args.device[functionName](args);
