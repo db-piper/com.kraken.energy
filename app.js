5d433b66085d48b55794efb06eef2b5b20e3789e
db-piper <202308407+db-piper@users.noreply.github.com>
2025-11-02 21:18:02 +0000

    In RunListener functionality use function references throughout

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const productTariff = require('./modules/productTariff');
  * DONE: In the pairing process confirm there is a live meter id available; if not return no devices
  * DONE: Device Repair Functionality
  * DONE: Work out how to "subclass" Tariff devices with different sets of capabilities.
- * TODO: Fix the single slot problem for Tracker Tariff 
+ * TODO: Fix the single slot problem for Tracker Tariff by counting <<today's slots>>
  * TODO: Work out and implement capability changes for single slot Tracker
  * TODO: Release new version with these changes
  * TODO: Research and understand dispatches on intelligent tariffs
@@ -39,25 +39,24 @@ module.exports = class krakenApp extends Homey.App {
 
   /**
    * Register the named function on the device class as the listener for the named condition flow card 
-   * @param {string}   cardName       The name of the condition card getting the listener
-   * @param {function} handlerFunction   The name of the function
+   * @param {string}   cardName           The name of the condition card getting the listener
+   * @param {function} handlerFunction    The function
    */
   registerConditionRunListener(cardName, handlerFunction) {
     this.homey.log(`krakenApp.registerConditionRunListener: card ${cardName} function: ${handlerFunction.name}`);
-    //TODO: Research if the function reference can be passed rather than the function name
-    this.homey.flow.getConditionCard(cardName).registerRunListener(this.runListenerExecutor.bind(this, handlerFunction.name));
+    this.homey.flow.getConditionCard(cardName).registerRunListener(this.runListenerExecutor.bind(this, handlerFunction));
   }
 
   /**
    * Run the named function in the context of the class specific in args.device with args as parameter
-   * @param {string} functionName   The name of the handler function
-   * @param {object} args           args.device is the device instance 
-   * @param {object} state          Current homey state 
+   * @param {function}  handlerFunction   The handler function
+   * @param {object}    args              args.device is the device instance 
+   * @param {object}    state             Current homey state 
    * @returns 
    */
-  async runListenerExecutor(functionName, args, state) {
-    this.homey.log(`krakenApp.runListenerExecutor: ${functionName}`)
-    const result = args.device[functionName](args);
+  async runListenerExecutor(handlerFunction, args, state) {
+    this.homey.log(`krakenApp.runListenerExecutor: ${handlerFunction.name}`)
+    const result = handlerFunction(args);
     return result;
   }
   
